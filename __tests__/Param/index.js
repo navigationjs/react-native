c96a0430cbdef7f98d46fee5088f0a88630d1b3a
Ivan Zotov <ivan_zotov@railsmob.com>
2019-10-27 20:41:10 +0300

    Add tests for Param to and loading

--- __tests__/Param/index.js
+++ __tests__/Param/index.js
@@ -26,4 +26,26 @@ describe('Param', () => {
     const param3 = new Param(1);
     expect(param3.loading).toBe(false)
   })
+
+  it('should change values on `to` method', async () => {
+    const param = new Param(0);
+    expect(param._value).toBe(0)
+    await param.to(0.5)
+    expect(param._value).toBe(0.5)
+    await param.to(1)
+    expect(param._value).toBe(1)
+  })  
+
+  it('should change loading when value hits 1 or 0', async () => {
+    const param = new Param(0);
+    expect(param.loading).toBe(true)
+    await param.to(0.7)
+    expect(param.loading).toBe(true)
+    await param.to(1)
+    expect(param.loading).toBe(false)
+    await param.to(0.3)
+    expect(param.loading).toBe(false)
+    await param.to(0)
+    expect(param.loading).toBe(true)
+  })
 });
