342022854ae076d9ec4da252a3c97dd18f96d06c
Ivan Zotov <ivan_zotov@railsmob.com>
2019-11-13 10:10:18 +0300

    Change website config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Test Site', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://ivanzotov.github.io', // Your website URL
+  baseUrl: '/react-native-animated-navigation/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'react-native-animated-navigation',
+  organizationName: 'ivanzotov',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
