a828dad79a04061a023e8bb60111369f72edd9d4
Ivan Zotov <git@ivanzotov.com>
2020-02-06 12:44:30 +0300

    Fix backgroundColor for Stack. Bump to 0.31.1

--- src/Stack/Wrap.js
+++ src/Stack/Wrap.js
@@ -61,7 +61,7 @@ export default class Wrap extends Component {
             position: 'absolute',
             width: '100%',
             height: '100%',
-            backgroundColor: 'white',
+            backgroundColor: 'transparent',
             overflow: 'hidden',
           },
           {
