d46421b1d19da1dac96f978c513bcb338759ceb6
Ivan Zotov <git@ivanzotov.com>
2020-02-14 11:52:01 +0300

    Revert last AndroidBack changes

--- src/AndroidBack/index.js
+++ src/AndroidBack/index.js
@@ -5,16 +5,12 @@ import navigation from '@navigationjs/core';
 export default class AndroidBack extends Component {
   componentDidMount() {
     const { id, onPress } = this.props;
-    if (id && onPress) {
-      navigation.on(events.id('android_back', id), onPress);
-    }
+    navigation.on(events.id('android_back', id), onPress);
   }
 
   componentWillUnmount() {
     const { id } = this.props;
-    if (id) {
-      navigation.off(events.id('android_back', id));
-    }
+    navigation.off(events.id('android_back', id));
   }
 
   render() {
