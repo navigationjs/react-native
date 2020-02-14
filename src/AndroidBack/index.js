d8d75ae127a649a119d570db29ae8060d0ae189d
Ivan Zotov <git@ivanzotov.com>
2020-02-14 11:29:43 +0300

    Do not crash if AndroidBack received undefined

--- src/AndroidBack/index.js
+++ src/AndroidBack/index.js
@@ -5,12 +5,16 @@ import navigation from '@navigationjs/core';
 export default class AndroidBack extends Component {
   componentDidMount() {
     const { id, onPress } = this.props;
-    navigation.on(events.id('android_back', id), onPress);
+    if (id && onPress) {
+      navigation.on(events.id('android_back', id), onPress);
+    }
   }
 
   componentWillUnmount() {
     const { id } = this.props;
-    navigation.off(events.id('android_back', id));
+    if (id) {
+      navigation.off(events.id('android_back', id));
+    }
   }
 
   render() {
